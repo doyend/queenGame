7c42c91e7ae6078d0589933e7dd1de0748e0b3f7
Yi Dong <doyend@gmail.com>
2017-02-24 22:52:50 -0500

    initial checkings

---
+++ js/game.js
@@ -0,0 +1,159 @@
+function shuffleArray(array)
+{
+    for (var i = array.length - 1; i > 0; i--)
+    {
+        var index = parseInt(Math.random() * (i + 1));
+        var temp = array[index];
+        array[index] = array[i];
+        array[i] = temp;
+    }
+}
+
+function Board(size){
+    this.scoreCompute = new ScoreCompute(size);
+    this.size = size;
+    this.board = [];
+    this.legal = this.scoreCompute.legalMoves(this.board);
+    this.computerTurn = false;
+
+    that = this;
+    this.clickHandler =  function(e){
+        var position = parseInt(this.id);
+        if (!this.computerTurn && that.legal.indexOf(position) >= 0) {
+            that.placeQueen(this, false);
+            that.updateBoardStatus();
+            if (that.legal.length == 0){
+                that.showMessage(true);
+            }
+            else{
+                that.computerMove();
+            }
+        }
+    };
+
+    this.onMouseOverHandler = function(e){
+        var position = parseInt(this.id);
+        if (that.board.indexOf(position) >= 0) {
+            return;
+        }
+        if (that.legal.indexOf(position) >= 0) {
+           this.style.backgroundColor = "green";
+        } else{
+           this.style.backgroundColor = "red";
+        }
+
+    };
+
+    this.onMouseOutHandler = function(e){
+        var position = parseInt(this.id);
+        this.style.backgroundColor = parseInt((position / boardSize) + position) % 2 == 0 ? '#ababab' : 'white';
+    };
+
+    this.setup();
+
+    if (!document.getElementById("form").children[0].checked){
+      that.computerMove();
+    }
+
+
+}
+
+Board.prototype.getDiv = function(i){
+    var root = document.getElementById("mainChessBoard");
+    var children = root.children;
+    return children[i];
+}
+
+Board.prototype.placeQueen = function(div, computer){
+    if (computer){
+           div.innerHTML="<img src='images/white.png'/>";
+    }
+    else{
+           div.innerHTML="<img src='images/black.png'/>";
+    }
+}
+
+Board.prototype.showMessage = function(win){
+    div = document.getElementById("message");
+    if (win){
+        div.innerHTML="<img src='images/win.png'/>";
+
+    }
+    else{
+        div.innerHTML="<img src='images/loss.png'/>";
+    }
+}
+
+Board.prototype.updateBoardStatus = function(){
+    this.board = [];
+    var root = document.getElementById("mainChessBoard");
+    var children = root.children;
+    var length = children.length;
+    for  (var i=0; i< length; i++){
+        if (children[i].innerHTML != ""){
+            this.board.push(parseInt(children[i].id));
+        }
+    }
+    this.legal = this.scoreCompute.legalMoves(this.board);
+}
+
+Board.prototype.computerMove = function(){
+    this.computerTurn = true;
+    var length = this.legal.length;
+    var best = -1000;
+    var position = null;
+    shuffleArray(this.legal);
+    for(var i=0; i < length; i++){
+        var pos = this.legal[i];
+        this.board.push(pos);
+        var score = this.scoreCompute.quickscore(this.board, false);
+        if (score > best){
+            best = score;
+            position = pos;
+        }
+        this.board.pop();
+    }
+    this.placeQueen(this.getDiv(position), true);
+    this.updateBoardStatus();
+    this.computerTurn = false;
+    if (this.legal.length == 0){
+        this.showMessage(false);
+    }
+}
+
+Board.prototype.setup = function(){
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var pixel = Math.min(w, h)- 50;
+    console.log(pixel);
+    var boardDiv = document.getElementById("mainChessBoard");
+    boardDiv.style.height = pixel+"px";
+    boardDiv.style.width = pixel+"px";
+    var boxSize = parseInt(pixel / this.size);
+    while (boardDiv.firstChild) {
+        boardDiv.removeChild(boardDiv.firstChild);
+    }
+    var messageDiv = document.getElementById("message");
+    while (messageDiv.firstChild) {
+        messageDiv.removeChild(messageDiv.firstChild);
+    }
+    for (var i=0; i< this.size * this.size; i++){
+        var divEle = (document.createElement("div"));
+        divEle.className += "box";
+        divEle.style.backgroundColor = parseInt((i / this.size) + i) % 2 == 0 ? '#ababab' : 'white';
+        divEle.style.height = boxSize+"px";
+        divEle.style.width = boxSize+"px";
+        divEle.id = i;
+        divEle.addEventListener("click", this.clickHandler);
+        divEle.addEventListener("mouseover", this.onMouseOverHandler);
+        divEle.addEventListener("mouseout", this.onMouseOutHandler);
+        boardDiv.appendChild(divEle);    
+    }
+}
+
+var boardSize = 8;
+var board = new Board(boardSize);
+function restart(){
+    board = new Board(boardSize);
+}
+document.getElementById("button").addEventListener("click", restart);
---
+++ js/work.js
@@ -0,0 +1,159 @@
+function ScoreCompute(boardSize){
+    this.boardSize = boardSize;
+
+    this.legalMoves = function(queens){
+        var total = queens.length;
+        var toberemoved = new Set();
+        for(var i = 0 ; i < total; i++){
+            var queen = queens[i];
+            var x = Math.floor(queen/this.boardSize);
+            var y = queen - x * this.boardSize;
+            var c0 = y - x;
+            var c1 = y + x;
+            var beg0 = Math.max(0, -c0);
+            var beg1 = Math.max(0, c1 - this.boardSize + 1);
+            var end0 = Math.min(this.boardSize - c0, this.boardSize);
+            var end1 = Math.min(this.boardSize, c1 + 1);
+            for ( var j = 0 ; j < this.boardSize; j++){
+                toberemoved.add(x * this.boardSize + j );
+                toberemoved.add(j * this.boardSize + y );
+            }
+            for ( var j = beg0; j < end0; j++){
+                toberemoved.add(j * this.boardSize + j + c0);
+            }
+            for ( var j = beg1; j < end1; j++){
+                toberemoved.add(j * this.boardSize - j + c1);
+            }
+        } 
+        var output = [];
+        total = this.boardSize * this.boardSize;
+        for(var i = 0 ; i < total; i++) {
+            if (!toberemoved.has(i)){
+                output.push(i);
+            }
+        }
+        return output;
+    }
+
+    function legalMoves2(oldLegals, queen){
+        var output = [];
+        var total = oldLegals.length;
+        if (queen >= 0){
+            var x = Math.floor(queen/this.boardSize);
+            var y = queen - x * this.boardSize;
+            var c0 = y - x;
+            var c1 = y + x;
+            for(var i = 0 ; i < total; i++) {
+                var potential = oldLegals[i];
+                var pX = Math.floor(potential/this.boardSize);
+                var pY = potential - pX * this.boardSize;
+                if ( pX == x ) continue; //removed, same line
+                if ( pY == y ) continue; //removed, same line
+                if (pY - pX == c0) continue; // same digonal
+                if (pX + pY == c1) continue; // same digonal
+                output.push(potential);
+            }
+        }
+        else{
+            for(var i = 0 ; i < total; i++) {
+                var potential = oldLegals[i];
+                output.push(potential);
+            }
+        }
+        return output;
+    }
+
+    function print(queens){
+        var str = '';
+        for(var i = 0; i < this.boardSize ; i++){
+            str += " |"
+            for(var j = 0; j < this.boardSize; j++){
+                var q = i * this.boardSize + j;
+                if (queens.indexOf(q) >= 0){
+                    str += "Q|";
+                }
+                else{
+                    str += ".|";
+                }
+
+            }
+            str += "\n";
+        }
+        console.log(str);
+    }
+
+    this.quickscore = function(queens, maxPlayer){
+        var legals = this.legalMoves(queens);
+        return score2(legals, -1, maxPlayer);
+    }
+
+    function score2(oldLegals, queen, maxPlayer){
+        var legals = legalMoves2(oldLegals, queen);
+        var total = legals.length;
+        if (total == 0){
+            if (maxPlayer){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        }
+        if (maxPlayer){
+            var best = -10000;
+            for( var i = 0 ; i < total ; i++){
+                queen = legals[i];
+                //queens.push(queen);
+                var v = score2(legals, queen, false);
+                //queens.pop();
+                best = Math.max(best, v);
+            }
+            return best;
+        }
+        else{
+            var best = 10000;
+            for( var i = 0 ; i < total ; i++){
+                queen = legals[i];
+                //queens.push(queen);
+                var v = score2(legals, queen, true);
+                //queens.pop();
+                best = Math.min(best, v);
+            }
+            return best;
+        }
+    }
+
+    this.score = function(queens, maxPlayer){
+        var legals = this.legalMoves(queens);
+        var total = legals.length;
+        if (total == 0){
+            if (maxPlayer){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        }
+        if (maxPlayer){
+            var best = -10000;
+            for( var i = 0 ; i < total ; i++){
+                queen = legals[i];
+                queens.push(queen);
+                var v = score(queens, false);
+                queens.pop();
+                best = Math.max(best, v);
+            }
+            return best;
+        }
+        else{
+            var best = 10000;
+            for( var i = 0 ; i < total ; i++){
+                queen = legals[i];
+                queens.push(queen);
+                var v = score(queens, true);
+                queens.pop();
+                best = Math.min(best, v);
+            }
+            return best;
+        }
+    }
+}
